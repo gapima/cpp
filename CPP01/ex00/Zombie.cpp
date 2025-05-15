cab37c1ff9c5d0de8e7b38685527c0de19568dec
gapima <gapima7@gmail.com>
2025-05-15 18:26:29 -0300

    cpp01 - ex01 finish

---
+++ CPP01/ex00/Zombie.cpp
@@ -0,0 +1,12 @@
+#include "Zombie.hpp"
+#include <iostream>
+
+Zombie::Zombie(std::string name) : _name(name) {}
+
+Zombie::~Zombie() {
+    std::cout << _name << " is destroyed." << std::endl;
+}
+
+void Zombie::announce() const {
+    std::cout << _name << ": BraiiiiiiinnnzzzZ..." << std::endl;
+}
